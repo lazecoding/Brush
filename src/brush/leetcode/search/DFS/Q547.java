3d71881152b5d5e83dea1c9cf62017fc778cc9c5
lazecoding <15062270668@163.com>
2020-12-15 21:47:00 +0800

    feat: 547-朋友圈

---
+++ src/brush/leetcode/search/DFS/Q547.java
@@ -0,0 +1,78 @@
+package brush.leetcode.search.DFS;
+
+/**
+ * @author: lazecoding
+ * @date: 2020/12/15 20:48
+ * @description:
+ */
+public class Q547 {
+    public static void main(String[] args) {
+        int[][] M = new int[][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        int ans = Solution.findCircleNum(M);
+        System.out.println(ans);
+    }
+
+    /**
+     * 班上有 N 名学生。其中有些人是朋友，有些则不是。他们的友谊具有是传递性。如果已知 A 是 B 的朋友，B 是 C 的朋友，那么我们可以认为 A 也是 C 的朋友。所谓的朋友圈，是指所有朋友的集合。
+     * <p>
+     * 给定一个 N * N 的矩阵 M，表示班级中学生之间的朋友关系。如果M[i][j] = 1，表示已知第 i 个和 j 个学生互为朋友关系，否则为不知道。你必须输出所有学生中的已知的朋友圈总数。
+     * <p>
+     * 示例 1：
+     * 输入：
+     * [[1,1,0],
+     * [1,1,0],
+     * [0,0,1]]
+     * 输出：2
+     * 解释：已知学生 0 和学生 1 互为朋友，他们在一个朋友圈。
+     * 第2个学生自己在一个朋友圈。所以返回 2 。
+     * <p>
+     * 示例 2：
+     * 输入：
+     * [[1,1,0],
+     * [1,1,1],
+     * [0,1,1]]
+     * 输出：1
+     * 解释：已知学生 0 和学生 1 互为朋友，学生 1 和学生 2 互为朋友，所以学生 0 和学生 2 也是朋友，所以他们三个在一个朋友圈，返回 1 。
+     *  
+     * <p>
+     * 提示：
+     * 1 <= N <= 200
+     * M[i][i] == 1
+     * M[i][j] == M[j][i]
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/friend-circles
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    static class Solution {
+
+        /**
+         *  无向图
+         *
+         * @param M
+         * @return
+         */
+        public static int findCircleNum(int[][] M) {
+            // 每个人，遍历，0表示未处理，1表示已处理
+            int[] visited = new int[M.length];
+            int count = 0;
+            for (int i = 0; i < M.length; i++) {
+                if (visited[i] == 0) {
+                    dfs(M, visited, i);
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static void dfs(int[][] M, int[] visited, int i) {
+            for (int j = 0; j < M.length; j++) {
+                // 有关系，而且没被处理
+                if (M[i][j] == 1 && visited[j] == 0) {
+                    visited[j] = 1;
+                    dfs(M, visited, j);
+                }
+            }
+        }
+    }
+}
