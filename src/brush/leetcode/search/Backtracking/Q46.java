e195a7ca7df64a3d17f7c22fca03e70ef3976cfb
lazecoding <15062270668@163.com>
2021-05-07 22:51:21 +0800

    feat :46-全排列

--- src/brush/leetcode/search/Backtracking/Q46.java
+++ src/brush/leetcode/search/Backtracking/Q46.java
@@ -1,6 +1,7 @@
 package brush.leetcode.search.Backtracking;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -38,13 +39,40 @@ public class Q46 {
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
     static class Solution {
+        /**
+         * 从左往右每一个位置都依此尝试填入一个数，看能不能填完这 n 个空格
+         * @param nums
+         * @return
+         */
         public static List<List<Integer>> permute(int[] nums) {
-            List<List<Integer>> ans = new ArrayList<>();
+            List<List<Integer>> res = new ArrayList<List<Integer>>();
 
+            List<Integer> output = new ArrayList<Integer>();
+            for (int num : nums) {
+                output.add(num);
+            }
 
+            int n = nums.length;
+            backtrack(n, output, res, 0);
+            return res;
+        }
 
-
-            return null;
+        public static void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+            // 所有数都填完了
+            if (first == n) {
+                res.add(new ArrayList<Integer>(output));
+            }
+            for (int i = first; i < n; i++) {
+                // 动态维护数组
+                Collections.swap(output, first, i);
+                // 继续递归填下一个数
+                backtrack(n, output, res, first + 1);
+                // 撤销操作
+                Collections.swap(output, first, i);
+            }
         }
     }
 }
+
+
+
